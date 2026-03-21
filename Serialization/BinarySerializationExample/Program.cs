cd4106b95a2184d827aae8188cd1811327f6794d
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-03-21 20:57:22 +0200

    Practice: Binary Serialization

---
+++ Serialization/BinarySerializationExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[Serializable]
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person() { }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
+internal static class BinarySerializationExample
+{
+    static void Main(string[] args)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        // Binary Serialization
+        using (FileStream stream = new FileStream("person.bin", FileMode.Create))
+        {
+            formatter.Serialize(stream, new Person("Mohamed", 18));
+        }
+
+        // Binary Deserialization
+        using (FileStream stream = new FileStream("person.bin", FileMode.Open))
+        {
+            Person deserializedPerson = (Person)formatter.Deserialize(stream);
+            Console.WriteLine($"Name: {deserializedPerson.Name}, Age: {deserializedPerson.Age}");
+        }
+    }
+}
+
