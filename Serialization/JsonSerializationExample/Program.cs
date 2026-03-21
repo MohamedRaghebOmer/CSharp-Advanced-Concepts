db96674814d3199a9258ad827c4390aded99b9d5
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-03-21 20:46:46 +0200

    Practice: JSON Serialization

---
+++ Serialization/JsonSerializationExample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+[Serializable]
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person() { }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
+internal static class JsonSerializationExample
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person("Mohamed", 18);
+
+        DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Person));
+
+        // JSON Serialization
+        using (MemoryStream stream = new MemoryStream())
+        {
+            // Serialize the person in JSON format and save in 'stream'
+            serializer.WriteObject(stream, person);
+
+            // Get the JSON string from 'stream' by converting it to binary and convert it again to string 
+            string jsonString = System.Text.Encoding.UTF8.GetString(stream.ToArray());
+
+            // Save the json string in a file (optional)
+            File.WriteAllText("person.json", jsonString);
+        }
+
+
+        // JSON Deserialization
+        using (FileStream stream = new FileStream("person.json", FileMode.Open))
+        {
+            // Deserialize the person
+            Person deserializedPerson = (Person)serializer.ReadObject(stream);
+
+            Console.WriteLine($"Name: {deserializedPerson.Name}, Age: {deserializedPerson.Age}");
+        }
+    }
+}
--- Serialization/XMLSerializationExample/Program.cs
+++ Serialization/XMLSerializationExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
 
@@ -8,26 +9,45 @@ public class Person
 {
     public string Name { get; set; }
     public int Age { get; set; }
+
+    public Person() { } // Serializer must find an empty constructor to be able to work
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
 }
 
 internal static class XMLSerializationExample
 {
     static void Main(string[] args)
     {
-        Person person = new Person { Name = "Mohamed Ragheb", Age = 18};
+        List<Person> people = new List<Person>();
+
+        for (int i = 1; i <= 100; i++)
+        {
+            people.Add(new Person($"Name{i}", i));
+        }
+
+        XmlSerializer serializer = new XmlSerializer(typeof(List<Person>));
 
         // Serialization
-        XmlSerializer serializer = new XmlSerializer(typeof(Person));
-        using (TextWriter writer = new StreamWriter("person.txt"))
+        using (TextWriter writer = new StreamWriter("People.xml"))
         {
-            serializer.Serialize(writer, person);
+            serializer.Serialize(writer, people);
         }
 
         // Deserialization
-        using (TextReader reader = new StreamReader("person.txt"))
+        List<Person> loadedPeople;
+        using (StreamReader reader = new StreamReader("People.xml"))
         {
-            Person deserializedPerson = (Person)serializer.Deserialize(reader);
-            Console.WriteLine($"Name: {deserializedPerson.Name}, Age: {deserializedPerson.Age}");
+            loadedPeople = (List<Person>)serializer.Deserialize(reader);
+        }
+
+        foreach (var p in loadedPeople)
+        {
+            Console.WriteLine($"Name: {p.Name}, Age: {p.Age}");
         }
     }
 }
