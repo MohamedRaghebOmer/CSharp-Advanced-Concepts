e6111a505d93fd8eaaf74ec51b7cd2cdc273aeff
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-03-21 14:29:40 +0200

    Practice: Func delegate

---
+++ EventsAndDelegates/FuncDelegate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FuncDelegate
+{
+    internal static class Program
+    {
+        static Func <int, int> SquareDelegate;
+
+        static int Square(int x)
+        {
+            return x * x;
+        }
+
+        static void Main(string[] args)
+        {
+            SquareDelegate = Square;
+            Console.WriteLine(SquareDelegate?.Invoke(5));
+        }
+    }
+}
