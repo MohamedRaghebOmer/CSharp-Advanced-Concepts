1dfc86ef470ea2a56052c50c90e9f5a281300723
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-03-21 17:18:29 +0200

    Practice:

---
+++ Other/NullableDataTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+internal static class NullableDataTypes
+{
+    static void Main(string[] args)
+    {
+        Nullable<int> nullableInt1 = null;
+        int? nullableInt2 = null;
+
+        if (nullableInt1.HasValue)
+        {
+            Console.WriteLine(nullableInt1.ToString());
+        }
+        else
+        {
+            Console.WriteLine("nullableInt1 is null");
+        }
+
+        if (nullableInt2.HasValue)
+        {
+            Console.WriteLine(nullableInt2.ToString());
+        }
+        else
+        {
+            Console.WriteLine("nullableInt2 is null");
+        }
+
+        int result = nullableInt2 ?? 0;
+        Console.WriteLine("result = " + result);
+
+        string stringValue = nullableInt1?.ToString();
+        Console.WriteLine($"stringValue = {stringValue}");
+    }
+}
+
---
+++ Other/UsingStatements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Mohamed = System.Console;
+using static System.Math;
+using System.Data.SqlClient;
+
+internal static class UsingStatements
+{
+    static void Main(string[] args)
+    {
+        Mohamed.WriteLine("This is a line.");
+
+        // We are able to use 'Sqrt' without 'Math' class name due to 'using static System.Math;'
+        Mohamed.WriteLine("Sqrt of 9 = " + Sqrt(9));
+
+        string connectionString = "Server=server_name;Database=database_name;User Id=user_id;Password=password;";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        using (SqlCommand command = new SqlCommand("SELECT FirstName FROM People WHERE PersonId = @PersonId;", connection))
+        {
+            command.Parameters.AddWithValue("@PersonId", 4);
+            connection.Open();
+
+            // using (SqlDataReader reader = command.ExecuteReader())
+            using (var reader = command.ExecuteReader())
+            {
+                string firstName = null;
+                if (reader.Read())
+                {
+                    firstName = reader["FirstName"].ToString();
+                }
+            }
+        }
+    }
+}
