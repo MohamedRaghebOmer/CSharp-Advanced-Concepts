dde16decc550e166eb24f648f6d356a03baa3732
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-03-21 20:04:17 +0200

    Practice: XML Serialization Example

---
+++ Serialization/XMLSerializationExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+
+[Serializable]
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+internal static class XMLSerializationExample
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person { Name = "Mohamed Ragheb", Age = 18};
+
+        // Serialization
+        XmlSerializer serializer = new XmlSerializer(typeof(Person));
+        using (TextWriter writer = new StreamWriter("person.txt"))
+        {
+            serializer.Serialize(writer, person);
+        }
+
+        // Deserialization
+        using (TextReader reader = new StreamReader("person.txt"))
+        {
+            Person deserializedPerson = (Person)serializer.Deserialize(reader);
+            Console.WriteLine($"Name: {deserializedPerson.Name}, Age: {deserializedPerson.Age}");
+        }
+    }
+}
