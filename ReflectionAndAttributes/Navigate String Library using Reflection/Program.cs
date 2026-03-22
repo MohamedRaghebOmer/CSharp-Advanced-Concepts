2af5551ab94639c63400d4e1031fd4e33273f4dd
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-03-22 19:59:08 +0200

    Practice: Reflection & Custom Attributes

---
+++ ReflectionAndAttributes/Navigate String Library using Reflection/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+static class Program // internal by default
+{
+    static void Main() // private by default
+    {
+        // Get the assembly containing the System.String type
+        Assembly assembly = typeof(string).Assembly;
+
+        // Get the System.String type
+        Type stringType = assembly.GetType("System.String");
+
+        if (stringType != null)
+        {
+            Console.WriteLine($"Methods of the System.String class:\n");
+
+            // Get all public methods of the System.String class
+            var stringMethods = stringType.GetMethods(BindingFlags.Public  | BindingFlags.Instance)
+                .OrderBy(method => method.Name);
+
+            foreach (var method in stringMethods)
+            {
+                Console.WriteLine($"\t{method.ReturnType} {method.Name}({GetParameterList(method.GetParameters())})");
+            }
+        }
+        else
+        {
+            Console.WriteLine("System.String type not found.");
+        }
+
+        Console.ReadKey();
+    }
+
+    static string GetParameterList(ParameterInfo[] parameters)
+    {
+        return string.Join(", ", parameters.Select(parameter => $"{parameter.ParameterType} {parameter.Name}"));
+    }
+}
