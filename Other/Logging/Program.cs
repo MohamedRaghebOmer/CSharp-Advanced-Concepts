64e22a9df8921019811190a3e7e16ef1845ee12b
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-03-24 17:34:52 +0200

    Learn & Pracitce: Windows Event Viewer Logging

---
+++ Other/Logging/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        // Specify source name for the event log
+        string sourceName = "MyAppName";
+
+        // Create the source if does not exist. 
+        if (!EventLog.SourceExists(sourceName))
+        {
+            EventLog.CreateEventSource(sourceName, "Application");
+            Console.WriteLine("Source created successfully.");
+        }
+
+        // Log an information message
+        EventLog.WriteEntry(sourceName, "This is an information message.", EventLogEntryType.Information);
+
+        // Log a warning message
+        EventLog.WriteEntry(sourceName, "This is a warning message.", EventLogEntryType.Warning);
+
+        // Log an error message
+        EventLog.WriteEntry(sourceName, "This is an error message.", EventLogEntryType.Error);
+    }
+}
