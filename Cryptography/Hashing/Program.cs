704c5a10a5c8b427921feea2c4c8010241442bb1
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-03-24 20:17:08 +0200

    Learn: Password hashing

---
+++ Cryptography/Hashing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+internal static class Hashing
+{
+    static string ComputeHash(string password)
+    {
+        using (SHA256 s = SHA256.Create())
+        {
+            byte[] hashBytes = s.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        bool again = true;
+
+        while (again)
+        {
+            Console.Write("Enter password to hash: ");
+            string password = Console.ReadLine();
+            Console.WriteLine($"Password after hashing is {ComputeHash(password)}");
+
+            Console.Write("\nDo you want to has another password [Y|N]? ");
+            again = string.Equals(Console.ReadLine(), "y", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ Other/App.config/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+
+static class AppConfig
+{
+    static void Main(string[] args)
+    {
+        string connectionString = ConfigurationManager.AppSettings["connectionString"];
+        string logLevel = ConfigurationManager.AppSettings["LogLevel"];
+        string Mohamed = ConfigurationManager.AppSettings["Mohamed"];
+        string DBConnectionString = ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+
+        Console.WriteLine("connection String: " + connectionString);
+        Console.WriteLine("log level: " + logLevel);
+        Console.WriteLine("Mohamed: " + Mohamed);
+        Console.WriteLine("DBConnectionString: " + DBConnectionString);
+    }
+}
