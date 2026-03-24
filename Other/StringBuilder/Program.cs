cb33762ba896c2db0e9d83de85563274e9968cfe
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-03-24 19:02:02 +0200

    Learn: String builder

---
+++ Other/StringBuilder/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+internal static class StringBuilderPractice
+{
+    static void ConcatenateWithNormalString(int numOfIteration)
+    {
+        string str = string.Empty;
+
+        for (int i = 0; i < numOfIteration; i++)
+        {
+            str += 'a';
+        }
+    }
+
+    static void ConcatenateWithBuilderString(int numOfIteration)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < numOfIteration; i++)
+        {
+            sb.Append('a');
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int numOfIteration = 209500;
+
+        Stopwatch builderStringStopWatch = new Stopwatch();
+        builderStringStopWatch.Start();
+        ConcatenateWithBuilderString(numOfIteration);
+        builderStringStopWatch.Stop();
+        Console.WriteLine($"Builder string take: {builderStringStopWatch.ElapsedMilliseconds}ms");
+
+        Stopwatch normalStrStopWatch = new Stopwatch();
+        normalStrStopWatch.Start();
+        ConcatenateWithNormalString(numOfIteration);
+        normalStrStopWatch.Stop();
+        Console.WriteLine($"Normal string take: {normalStrStopWatch.ElapsedMilliseconds}ms");
+    }
+}
+
