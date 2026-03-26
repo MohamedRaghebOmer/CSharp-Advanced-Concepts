94387d36804657e7d93816a81cd30a842eec4514
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-03-26 10:14:31 +0200

    Practice: TrafficLightProject project

---
+++ PracticeProjects/TrafficLightProject/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Forms;
+
+namespace TrafficLightProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            ctrlTrafficLight1.Start();
+        }
+
+        private void ctrlTrafficLight1_OnLightIsGreen()
+        {
+            MessageBox.Show("Green");
+        }
+
+        private void ctrlTrafficLight1_OnLightIsRed()
+        {
+            MessageBox.Show("Red");
+        }
+
+        private void ctrlTrafficLight1_OnLightIsYellow()
+        {
+            MessageBox.Show("Yellow");
+        }
+    }
+}
---
+++ PracticeProjects/TrafficLightProject/ctrlTrafficLight.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace TrafficLightProject
+{
+    partial class ctrlTrafficLight
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.LightTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.pbTrafficLight = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTrafficLight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.Red;
+            this.lblTime.Location = new System.Drawing.Point(59, 201);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(39, 29);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Tag = "";
+            this.lblTime.Text = "10";
+            // 
+            // LightTimer
+            // 
+            this.LightTimer.Enabled = true;
+            this.LightTimer.Interval = 1000;
+            this.LightTimer.Tick += new System.EventHandler(this.LightTimer_Tick);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Image = global::TrafficLightProject.Properties.Resources.Reset;
+            this.btnReset.Location = new System.Drawing.Point(74, 235);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(40, 32);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Image = global::TrafficLightProject.Properties.Resources.Pause;
+            this.btnPause.Location = new System.Drawing.Point(28, 235);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(40, 32);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // pbTrafficLight
+            // 
+            this.pbTrafficLight.Image = global::TrafficLightProject.Properties.Resources.Red;
+            this.pbTrafficLight.Location = new System.Drawing.Point(3, 3);
+            this.pbTrafficLight.Name = "pbTrafficLight";
+            this.pbTrafficLight.Size = new System.Drawing.Size(136, 195);
+            this.pbTrafficLight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbTrafficLight.TabIndex = 0;
+            this.pbTrafficLight.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Image = global::TrafficLightProject.Properties.Resources.Play;
+            this.btnStart.Location = new System.Drawing.Point(28, 235);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(40, 32);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // ctrlTrafficLight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.pbTrafficLight);
+            this.Controls.Add(this.btnStart);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ctrlTrafficLight";
+            this.Size = new System.Drawing.Size(142, 270);
+            ((System.ComponentModel.ISupportInitialize)(this.pbTrafficLight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbTrafficLight;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer LightTimer;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
